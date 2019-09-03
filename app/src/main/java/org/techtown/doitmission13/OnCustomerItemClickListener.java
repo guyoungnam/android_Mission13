5469a1701990cb0307e88f7a24ed6e108faedd7d
guyoungnam <eak12@naver.com>
2019-09-03 18:22:59 +0900

    "리사이클러뷰에 고객정보 추가하기"

---
+++ app/src/main/java/org/techtown/doitmission13/OnCustomerItemClickListener.java
@@ -0,0 +1,12 @@
+package org.techtown.doitmission13;
+
+import android.view.View;
+
+public interface OnCustomerItemClickListener {
+
+    public void onItemClick(CustomerAdapter.ViewHolder holder, View view, int position);
+
+    }
+
+
+
